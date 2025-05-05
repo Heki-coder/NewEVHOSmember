e6c95b6f5557d113ce69e0b78342f92def67211c
Heki-coder <victor@ines-thorsten.de>
2025-05-05 16:50:02 +0200

    Große Sicherheitslücke zu beheben

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const TOKEN = '7343849648:AAH0W1YwoFz8PZH5i9Ixevdt0NiZOjTw494';
-const chatID = '1113713328';
-function sendTelegramMessage(message) {
-    fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            chat_id: chatID,
-            text: message
-        })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Message sent successfully:', data);
-    })
-}
-function klick_button() {
-    const name = document.getElementById('name');
-    const info = document.getElementById('info');
-    sendTelegramMessage(`Ich heiße ${name.value}`);
-    setTimeout(function() {
-        sendTelegramMessage(`Informationen: ${info.value}`);
-        name.value = '';
-        info.value = '';
-    }, 1000);
 
-}
+// const chatID = '1113713328';
+// function sendTelegramMessage(message) {
+//     fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             chat_id: chatID,
+//             text: message
+//         })
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log('Message sent successfully:', data);
+//     })
+// }
+// function klick_button() {
+//     const name = document.getElementById('name');
+//     const info = document.getElementById('info');
+//     sendTelegramMessage(`Ich heiße ${name.value}`);
+//     setTimeout(function() {
+//         sendTelegramMessage(`Informationen: ${info.value}`);
+//         name.value = '';
+//         info.value = '';
+//     }, 1000);
+
+// }
